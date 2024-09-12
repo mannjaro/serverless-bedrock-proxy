1324af0a04eb047f7482ca724a4ac0c034056871
manjaro <takuson2525zt@gmail.com>
2024-09-13 08:04:57 +0900

    add iam policy

--- lib/construct/lambda.ts
+++ lib/construct/lambda.ts
@@ -1,23 +1,33 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class ProxyLambda extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id);
+    const role = new iam.Role(this, "Role", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole",
+      ),
+    );
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonBedrockFullAccess"),
+    );
+
     const fn = new NodejsFunction(this, "lambda", {
       entry: "lambda/bedrock-proxy/index.ts",
       handler: "handler",
       runtime: lambda.Runtime.NODEJS_20_X,
+      role: role,
     });
     fn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
       invokeMode: lambda.InvokeMode.RESPONSE_STREAM,
-    });
-    new apigw.LambdaRestApi(this, "api", {
-      handler: fn,
     });
   }
 }
